79586daa61e415584d0cbe50343f5da02622527a
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-26 18:46:28 -0400

    WORKING API

--- models/_db.js
+++ models/_db.js
@@ -1,4 +1,4 @@
 'use strict';
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/bookstore', {logging: false});
+var db = new Sequelize('postgres://localhost:5432/bookstoreapi', {logging: false});
 module.exports = db;
