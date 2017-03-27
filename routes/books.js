327fc561bda3ea082b86ef9f06d0bcd54f95509f
TinaHeiligers <tina.heiligers@gmail.com>
2017-03-27 09:08:06 -0400

    getting routes to play with templates

--- routes/books.js
+++ routes/books.js
@@ -13,23 +13,17 @@ module.exports = router;
 router.get('/', function(req, res, next) {
     Book.findAll({})
     .then(function(books) {
-        res.json({books: books});
+        // res.json({books: books});
+        res.render('booklist', {
+            books: books
+        })
     }).catch(next);
 });
 
-//Create a book without an Author, add book to db, adding author to db not working yet through the form.
-// router.post('/', function(req, res, next) {
-//     Book.create(req.body)
-//     .then(function(createdBook) {
-//         console.log('book created', createdBook.dataValues);
-//         //create a book page to render out here
-//         res.json(createdBook);
-//     })
-//     .catch(next);
-// });
 
+//POST a book, this route works
+//hard coding the association, setAuthor does not seem to work
 router.post('/', function(req, res, next) {
-    //currently, if the author does not exist, this bombs out.
     // console.log(req.body.name, req.body.language);
     Author.findOrCreate({
         where: {
@@ -38,7 +32,6 @@ router.post('/', function(req, res, next) {
         }
     })
     .spread(function(returnedAuthor, createdBookBool) {
-        // console.log('returnedAuthor: ?????');
         console.log(returnedAuthor.dataValues);
         return Book.create({
             title: req.body.title,
@@ -56,6 +49,65 @@ router.post('/', function(req, res, next) {
         })
         .catch(next);
     });
+});
+//GET a book by id:
+router.get('/book/:id', function(req, res, next) {
+    Book.findById(req.params.id)
+        .then(function(foundBook) {
+            res.status(200);
+            res.render('booktitle', {
+                book: foundBook
+            });
+        })
+        .catch(next)
+});
+
+//change book title
+router.put('/:id', function(req, res, next) {
+    Book.update(req.body, {
+        where: {id: req.params.id},
+        returning: true
+    })
+    .then(function(results) {
+        var updatedBook = results[1][0];
+        res.json({
+            book: updatedBook
+        });
+    })
+    .catch(next);
+});
+
+//delete ONE book
+router.delete('/:id', function(req, res, next) {
+    //find the book,
+    Book.findById(req.params.id)
+    .then(function(foundBook) {
+        foundBook.destroy();
+    })
+    .then(function() {
+        res.status(200).send();
+    })
+    .catch(next);
+});
+
+//For searching through books from the front-end:
+router.get('/search', function (req, res, next) {
+
+    Book.findByCategory(req.query.search)
+        .then(function (books) {
+            res.render('index', {
+                books: books
+            });
+        })
+        .catch(next);
+
+});
+
+//For front-end: add form for adding a book
+router.get('/add', function(req, res) {
+    res.status(200);
+    console.log("rendering out the addBook page");
+    res.render('addBook');
 });
 
 function generateError (message, status) {
@@ -81,60 +133,4 @@ router.get('/book/:title', function (req, res, next) {
     })
     .catch(next);
 
-});
-
-router.get('/search', function (req, res, next) {
-    Book.findbByCategory(req.query.search)
-        .then(function(books) {
-            res.render('index', {
-                books: books
-            });
-        })
-        .catch(next);
-})
-
-//change book title
-router.put('/:id', function(req, res, next) {
-    Book.update(req.body, {
-        where: {id: req.params.id},
-        returning: true
-    })
-    .then(function(results) {
-        var updatedBook = results[1][0];
-        res.json({
-            book: updatedBook
-        });
-    })
-    .catch(next);
-});
-//delete ONE book
-router.delete('/:id', function(req, res, next) {
-    //find the book,
-    Book.findById(req.params.id)
-    .then(function(foundBook) {
-        foundBook.destroy();
-    })
-    .then(function() {
-        res.status(200).send();
-    })
-    .catch(next);
-});
-//For searching through books from the front-end:
-router.get('/search', function (req, res, next) {
-
-    Book.findByCategory(req.query.search)
-        .then(function (books) {
-            res.render('index', {
-                books: books
-            });
-        })
-        .catch(next);
-
-});
-
-//For front-end: add form for adding a book
-router.get('/add', function(req, res) {
-    res.status(200);
-    console.log("rendering out the addBook page");
-    res.render('addBook');
 });
